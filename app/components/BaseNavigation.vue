1346ffc4226a5660b71e02da9000682061cfad5c
Aaron193 <76023256+Aaron193@users.noreply.github.com>
2025-06-06 18:47:43 -0400

    inline svg because nuxt is not cooperating

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -78,13 +78,17 @@ onUnmounted(() => {
                 <div class="absolute inset-0 rounded-xl bg-gradient-to-r from-amber-500 to-red-500 p-0.5">
                     <div class="w-full h-full bg-zinc-950 rounded-xl"></div>
                 </div>
-                <NuxtImg
-                    :src="isMobileMenuOpen ? '/close.svg' : '/menu.svg'"
-                    width="24"
-                    height="24"
-                    class="relative text-zinc-200 hover:text-amber-500 transition-colors"
-                    draggable="false"
-                />
+                <span class="relative text-zinc-200 hover:text-amber-500 transition-colors">
+                    <svg v-if="isMobileMenuOpen" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path
+                            d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
+                            fill="white"
+                        />
+                    </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                        <path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" fill="white" />
+                    </svg>
+                </span>
             </button>
         </div>
 
